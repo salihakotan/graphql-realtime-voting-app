3b7e1eb6715af7fd234d9d6793a06ca2141ff06d
Saliha Kotan <salihakotan77@gmail.com>
2024-06-21 00:04:23 +0300

    footer info

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     <div>
         <Container maxW='100%' bg='pink.600' centerContent>
   <Box padding='4' bg='pink.500' color='white' maxW='80%'>
-  This project assignment has been prepared for the 'Kodluyoruz' GraphQL course. It is a real-time voting application that includes both backend and client and works with GraphQL.
+  This project assignment has been prepared by Saliha Kotan for the 'Kodluyoruz' GraphQL course. It is a real-time voting application that includes both backend and client and works with GraphQL.
 
   </Box>
 </Container>
